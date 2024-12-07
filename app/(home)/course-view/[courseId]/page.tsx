8133305a876a00a77abff57979830000494975ca
unknown <sium1206@gmail.com>
2024-12-08 01:55:42 +0600

    alhamdulilah,working on corse details page

--- app/(home)/course-view/[courseId]/page.tsx
+++ app/(home)/course-view/[courseId]/page.tsx
@@ -1,9 +1,11 @@
 import React from "react";
-
+import { BlocksRenderer, type BlocksContent } from "@strapi/blocks-react-renderer";
+import Image from "next/image";
+import Test from "../../../../assets/test.jpg";
+import CustomBtn from "@/components/customBtn";
 const SingleCourse = async ({ params }) => {
-  let { courseId } = params;
-
-  let res = await fetch(`${process.env.NEXT_PUBLIC_starpi_url}/single-courses?filters[id][$in]=${courseId}&populate[course_thumbnail][populate]=*&populate[course_lessons][populate]=lesson_assets`, {
+  let { courseId } = await params;
+  let res = await fetch(`${process.env.NEXT_PUBLIC_starpi_url}/single-courses?filters[documentId][$eqi]=${courseId}&populate[course_thumbnail][populate]=*&populate[course_lessons][populate]=lesson_assets`, {
     method: "GET",
     headers: {
       Authorization: `Bearer ${process.env.NEXT_PUBLIC_strapi_api_token}`,
@@ -11,9 +13,36 @@ const SingleCourse = async ({ params }) => {
   });
 
   let singleCouseRes = await res.json();
-  console.log("singleCouseRes data", singleCouseRes);
-
-  return <div>SingleCourseShower</div>;
+  console.log("singleCouseRes data", singleCouseRes.data[0].course_description);
+  const content: BlocksContent = singleCouseRes.data[0].course_description;
+  return (
+    <div style={{ width: "90%", margin: "auto" }} className="course-details__wrapper">
+      <div className="course-details__header">
+        <div>
+          <div>
+            <p className="course-details__title">Extracting a color palette from an image with javascript</p>
+            <div className="course-details__header-course-metadata">
+              <CustomBtn btnName="Get Started" btnColor="white" btnTextColor="black" />
+              <p>4.5 ⭐⭐⭐⭐⭐ </p>
+              <p>12152 Ratings</p>
+              <p>12323 Students</p>
+            </div>
+          </div>
+          <div style={{ backgroundColor: "blue" }}>
+            <Image alt="tesxt" src={Test} height={500} width={500} style={{ height: "100%", width: "100%" }} />
+          </div>
+        </div>
+      </div>
+      <div className="course-details__body">
+        <div className="course-details__body-left-column">
+          <div className="course-details__body-description">
+            <BlocksRenderer content={content} />
+          </div>
+        </div>
+        <div className="course-details__body-right-column">div.</div>
+      </div>
+    </div>
+  );
 };
 
 export default SingleCourse;
--- components/customBtn.tsx
+++ components/customBtn.tsx
@@ -5,7 +5,7 @@ import React from "react";
 const CustomBtn = ({ btnName, btnTextColor, btnColor }: { btnName: string; btnTextColor: string; btnColor: string }) => {
   const router = useRouter();
   return (
-    <div onClick={() => router.push("/course-view/4")} style={{ padding: ".5rem 1.5rem", backgroundColor: btnColor, borderRadius: "3%", margin: "1% 0%", cursor: "pointer" }}>
+    <div onClick={() => router.push("/course-view/zp1qkxn1ardt5goe0beafprf")} style={{ padding: ".5rem 1.5rem", backgroundColor: btnColor, borderRadius: "3%", margin: "1% 0%", cursor: "pointer" }}>
       <p style={{ color: btnTextColor, textAlign: "center" }}>{btnName}</p>
     </div>
   );

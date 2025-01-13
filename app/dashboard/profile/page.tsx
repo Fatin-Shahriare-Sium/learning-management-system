5afa3e636feb840f68972299a0cadac125b6a693
unknown <sium1206@gmail.com>
2025-01-13 12:35:24 +0600

    alhamdulillah,complete profile section of dashboard

--- app/dashboard/profile/page.tsx
+++ app/dashboard/profile/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 import InputBox from "@/components/inputBox";
-import React, { useState } from "react";
+import { useAppProvider } from "@/context/appProvider";
+import React, { cache, useEffect, useState } from "react";
 
 const ProfileEditor = () => {
+  const { token } = useAppProvider();
   const [formData, setFormData] = useState({
     name: "",
     phone: "",
@@ -11,6 +13,27 @@ const ProfileEditor = () => {
 
   const [preview, setPreview] = useState(null);
   const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [picUnchanged, setPicUnchanged] = useState(true);
+
+  useEffect(() => {
+    fetch(`${process.env.NEXT_PUBLIC_starpi_url}/users/me`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        let { username, phoneNumber, profilePic } = data;
+        if (profilePic) {
+          setPreview(`${process.env.NEXT_PUBLIC_starpi_url_media}/${profilePic}`);
+        }
+        setFormData({ name: username, phone: phoneNumber, profilePicture: profilePic });
+      });
+  }, []);
 
   const handleChangeName = (value) => {
     setFormData({ ...formData, name: value });
@@ -20,30 +43,58 @@ const ProfileEditor = () => {
   };
 
   const handleFileChange = (e) => {
+    e.preventDefault();
     const file = e.target.files[0];
+
     if (file) {
       setFormData({ ...formData, profilePicture: file });
       setPreview(URL.createObjectURL(file));
+      setPicUnchanged(false);
     }
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
+    setLoading(true);
     const formDataToSend = new FormData();
     formDataToSend.append("name", formData.name);
     formDataToSend.append("phone", formData.phone);
-    if (formData.profilePicture) {
+
+    //upload pic start
+    if (picUnchanged == false) {
+      const formDataPicToSend = new FormData();
+      formDataPicToSend.append("files", formData.profilePicture);
+      const res = await fetch(`${process.env.NEXT_PUBLIC_starpi_url}/upload`, {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+        body: formDataPicToSend,
+      });
+      const data = await res.json();
+
+      console.log("data pic", data);
+      const picUrl = data[0].url;
+      formDataToSend.append("profilePicture", picUrl);
+
+      //upload pic end
+    } else {
       formDataToSend.append("profilePicture", formData.profilePicture);
     }
 
     try {
-      const response = await fetch("/api/update-profile", {
-        method: "POST",
+      let res = await fetch(`${process.env.NEXT_PUBLIC_starpi_url}/user/me`, {
+        method: "PUT",
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
         body: formDataToSend,
       });
+      let data = await res.json();
+      setLoading(false);
+      console.log("data after update ", data);
 
-      if (response.ok) {
+      if (data) {
         setMessage("Profile updated successfully!");
       } else {
         setMessage("Failed to update profile. Please try again.");
@@ -55,14 +106,14 @@ const ProfileEditor = () => {
 
   return (
     <div style={{ maxWidth: "400px", margin: "0 auto", color: "white" }}>
-      <h2>Edit Profile</h2>
+      <h2 style={{ fontSize: "2rem", fontWeight: "bold", textAlign: "center" }}>Edit Profile</h2>
       {message && <p>{message}</p>}
       <form onSubmit={handleSubmit}>
         <div style={{ marginBottom: "1rem" }}>
-          <InputBox title="Name" placeholder="your name" type="text" handleInputBox={handleChangeName} />
+          <InputBox title="Name" placeholder="your name" type="text" defaultValue={formData.name} handleInputBox={handleChangeName} />
         </div>
         <div style={{ marginBottom: "1rem" }}>
-          <InputBox title="Phone Number" placeholder="number" type="number" handleInputBox={handleChangePhone} />
+          <InputBox title="Phone Number" placeholder="number" type="number" defaultValue={formData.phone} handleInputBox={handleChangePhone} />
         </div>
         <div style={{ marginBottom: "1rem" }}>
           <label style={{ fontSize: "2rem" }}>
@@ -76,7 +127,13 @@ const ProfileEditor = () => {
             <img src={preview} alt="Profile Preview" style={{ width: "100px", height: "100px", borderRadius: "50%" }} />
           </div>
         )}
-        <button type="submit">Update Profile</button>
+        {loading ? (
+          <p>Loading ...</p>
+        ) : (
+          <button style={{ backgroundColor: "var(--section-color)", color: "white", padding: "1rem 2rem", border: "none" }} type="submit">
+            Update Profile
+          </button>
+        )}
       </form>
     </div>
   );
--- components/inputBox.tsx
+++ components/inputBox.tsx
@@ -3,13 +3,14 @@ interface inputBox {
   title: string;
   placeholder: string;
   type: string;
+  defaultValue: any;
   handleInputBox: () => any;
 }
-const InputBox = ({ title, placeholder, type, handleInputBox }: inputBox) => {
+const InputBox = ({ title, placeholder, type, handleInputBox, defaultValue }: inputBox) => {
   return (
     <div className="inputbox-wrapper">
       <p>{title}</p>
-      <input style={{ color: "white" }} type={type} onChange={(e) => handleInputBox(e.target.value)} placeholder={placeholder} />
+      <input style={{ color: "white" }} type={type} defaultValue={defaultValue} onChange={(e) => handleInputBox(e.target.value)} placeholder={placeholder} />
     </div>
   );
 };
